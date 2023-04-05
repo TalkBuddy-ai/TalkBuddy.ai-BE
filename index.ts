32568da1ed897548cf7e4d02ac824cc52b309197
Lama Ibrahim <librahim@asaltech.com>
2023-04-05 14:46:27 +0200

    update index.ts file

--- index.ts
+++ index.ts
@@ -1,15 +1,27 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
-
-dotenv.config();
+import cors from 'cors';
+import http from 'http';
+import routes from './src/routes/index.js';
+import cookieParser from "cookie-parser";
+import "dotenv/config";
 
 const app: Express = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/api/v1', routes);
+
 const port = process.env.PORT;
+
+const server = http.createServer(app);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
 
-app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-  });
+server.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
